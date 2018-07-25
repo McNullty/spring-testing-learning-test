4f8dd4ecee846ef8d29526d56354387b83d29b6a
Mladen Čikara <mladen.cikara@gmail.com>
2018-07-25 21:26:01 +0200

    Adding spring documentation to test

--- src/test/java/com/mladen/cikara/spring/testing/WebMockTest.java
+++ src/test/java/com/mladen/cikara/spring/testing/WebMockTest.java
@@ -2,24 +2,29 @@ package com.mladen.cikara.spring.testing;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
 @WebMvcTest(controllers = { GreetingController.class })
 public class WebMockTest {
 
-  @Autowired
   private MockMvc mockMvc;
 
   @MockBean
@@ -41,9 +46,18 @@ public class WebMockTest {
       .perform(get("/greeting"))
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(content().string(containsString("Hello Mock")));
+        .andExpect(content().string(containsString("Hello Mock")))
+        .andDo(document("greeting"));
 
     // @formatter:on
 
+  }
+
+  @BeforeEach
+  public void setUp(final WebApplicationContext webApplicationContext,
+      final RestDocumentationContextProvider restDocumentation) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation))
+        .build();
   }
 }
