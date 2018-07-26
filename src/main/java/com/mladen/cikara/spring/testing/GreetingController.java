b5c150bb711b6b2355bbcf588be9af21983cabc0
Mladen Čikara <mladen.cikara@gmail.com>
2018-07-26 23:24:27 +0200

    Added logging and waiting to see latency in JMeter tests

--- src/main/java/com/mladen/cikara/spring/testing/GreetingController.java
+++ src/main/java/com/mladen/cikara/spring/testing/GreetingController.java
@@ -1,11 +1,15 @@
 package com.mladen.cikara.spring.testing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
 
   private final GreetingService service;
 
@@ -16,6 +20,15 @@ public class GreetingController {
   @RequestMapping("/greeting")
   public @ResponseBody
   String greeting() {
+    LOGGER.info("Greeting endpoint");
+
+    try {
+      Thread.sleep(2000);
+    } catch (final InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
     return this.service.greet();
   }
 }
